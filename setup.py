ea9df8a995db2977a306d27b52de0da697f4f2db
NathanEpstein <ne2210@columbia.edu>
2016-02-23 16:49:40 -0500

    use setup.py to install requirements

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+  name = "Dora",
+  version = "0.0.1",
+  author = "Nathan Epstein",
+  author_email = "ne2210@columbia.edu",
+  description = ("Exploratory data analysis toolkit for Python"),
+  license = "MIT",
+  keywords = "exploratory data analysis",
+  install_requires = [
+    "pandas>=0.17.1",
+    "numpy>=1.10.4",
+    "scipy>=0.17.0",
+    "sklearn",
+    "matplotlib>=1.5.1"
+  ]
+)
