12be30d855819835a5da034a0bd5e653b942da13
nathanepstein <_@nepste.in>
2020-06-30 07:31:11 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name = "Dora",
-  version = "0.0.2",
+  version = "0.0.3",
   author = "Nathan Epstein",
   author_email = "ne2210@columbia.edu",
   description = ("Exploratory data analysis toolkit for Python"),
